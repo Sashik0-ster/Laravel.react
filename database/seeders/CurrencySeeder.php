c50d64451b7ddb1742dbc1dc395cc4e40e69c2b0
Sashik0-ster <124570370+Sashik0-ster@users.noreply.github.com>
2026-05-16 22:31:51 +0200

    Add CurrencySeeder to seed currency data

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,25 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Finance\Currency;
+use Illuminate\Database\Seeder;
+
+class CurrencySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $currencies = [
+            ['code' => 'USD', 'currency_name' => 'US Dollar', 'symbol' => '$', 'is_active' => true],
+            ['code' => 'EUR', 'currency_name' => 'Euro', 'symbol' => '€', 'is_active' => true],
+            ['code' => 'UAH', 'currency_name' => 'Ukrainian Hryvnia', 'symbol' => '₴', 'is_active' => true],
+            ['code' => 'PLN', 'currency_name' => 'Polish Zloty', 'symbol' => 'zł', 'is_active' => true],
+        ];
+
+        foreach ($currencies as $currency) {
+            Currency::firstOrCreate(
+                ['code' => $currency['code']],
+                $currency
+            );
+        }
+    }
+}
