c3aeb29d2d7279e87dcc84422888d8b4b2a557ec
Sashik0-ster <124570370+Sashik0-ster@users.noreply.github.com>
2026-05-16 22:33:58 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 namespace Database\Seeders;
+
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,5 +18,7 @@ class DatabaseSeeder extends Seeder
                 'password' => bcrypt('111111111'),
             ]
         );
+
+        $this->call(CurrencySeeder::class);
     }
 }
